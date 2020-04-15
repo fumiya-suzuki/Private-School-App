aafcb627f3f5893fa83fa48f224bc3ab6994efa6
fumiya-suzuki <fumiya.89.1216@gmail.com>
2020-04-15 09:33:42 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  def index
+    
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :tel, :number, :school, :grade, :number, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :grade, presence: true
   validates :school, presence: true
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
