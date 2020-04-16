22c4d6dede9f3145b42838dc0a3e92e0fed64ceb
fumiya-suzuki <fumiya.89.1216@gmail.com>
2020-04-16 11:46:21 +0000

    Finish full-fledget-login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200415072321) do
+ActiveRecord::Schema.define(version: 20200416104340) do
 
   create_table "users", force: :cascade do |t|
     t.integer "number"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200415072321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["number"], name: "index_users_on_number", unique: true
   end
 
