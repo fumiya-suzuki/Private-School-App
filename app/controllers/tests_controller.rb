45b6439c3f9a6b2a39c2bc576f5c397536e63e2b
fumiya-suzuki <fumiya.89.1216@gmail.com>
2020-05-06 02:23:39 +0000

    finish test edit and delete

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,6 +4,22 @@ class TestsController < ApplicationController
     @test = Test.new
   end
   
+  def edit 
+    @user = User.find(params[:user_id])
+    @test = Test.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:user_id])
+    @test = Test.find(params[:id])
+    if @test.update_attributes(test_params)
+      flash[:success] = "テスト結果を編集しました。"
+    else
+      flash[:danger] = "テスト結果編集に失敗しました。"
+    end
+    redirect_to user_tests_path(@user)
+  end
+  
   def create
     @user = User.find(params[:user_id])
     @test = @user.tests.new(test_params)
@@ -18,14 +34,22 @@ class TestsController < ApplicationController
   
   def index 
     @user = User.find(params[:user_id])
-    @tests = Test.where(user_id: @user.id)
-    @demo_tests = Test.where(user_id: @user.id, types: 2)
-    @periodic_tests = Test.where(user_id: @user.id, types: 1)
+    @tests = Test.where(user_id: @user.id).order(day: "ASC")
+    @demo_tests = Test.where(user_id: @user.id, types: 2).order(day: "ASC")
+    @periodic_tests = Test.where(user_id: @user.id, types: 1).order(day: "ASC")
   end
   
   def show_subject
     @user = User.find(params[:id])
     @test = Test.find(params[:id])
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    @test = Test.find(params[:id])
+    @test.destroy
+    flash[:success] = "#{@test.name}のデータを削除しました。"
+    redirect_to user_tests_url(@user)
   end
   
   
